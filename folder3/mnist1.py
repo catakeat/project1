da8874adbb900677dc1c6d52a46ca3ad2c98b9c5
catakeat <arsulescucatalin@yahoo.com>
2023-09-12 23:09:16 +0300

    Initial2

--- folder3/mnist1.py
+++ folder3/mnist1.py
@@ -2,6 +2,7 @@ from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', version=1,parser='auto')
 X,y = mnist['data'],mnist['target']
+print()
 print(X.shape)
 print(X.head(7))
 print(y.shape)
--- folder3/mnist2.py
+++ folder3/mnist2.py
@@ -1,6 +1,11 @@
-from mnist1 import X
+from sklearn.datasets import fetch_openml
 import matplotlib as plt
 
+mnist = fetch_openml('mnist_784', version=1,parser='auto')
+X,y = mnist["data"],mnist["target"]
+
+
 print("In mnist2")
-some_digit = X.iloc[:,0]
-print(some_digit)
+some_digit = X[0]
+some_digit_image = some_digit.reshape(28,28)
+print(some_digit_image)
