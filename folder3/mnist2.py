3bdc86b3b8f227f66cdb310af9e6448468ead96d
catakeat <catalin.arsulescu@yahoo.com>
2023-09-13 16:09:15 +0300

    commit la MNIST2

--- folder3/mnist2.py
+++ folder3/mnist2.py
@@ -1,12 +1,25 @@
 from sklearn.datasets import fetch_openml
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 mnist = fetch_openml('mnist_784', version=1,parser='auto')
 X,y = mnist["data"],mnist["target"]
-
-
+'''
+print(X.shape)
+print(y.shape)
+print(y.info())
+print(y.head())
+'''
 print("In mnist2")
-some_digit = X[0]
-some_digit_image = some_digit.reshape(28,28)
+print(X.info())
+#jos e o coloana
+#some_digit = X.iloc[0]
+
+#jos e un rind
+some_digit = X.loc[0]
+#print(some_digit)
+some_digit_image = some_digit.values.reshape(28,28)
 print(some_digit_image)
-test
+
+plt.imshow(some_digit_image, cmap = "binary", interpolation="nearest")
+plt.axis("off")
+plt.show()
